dac7e9f58af2969b3693631daf8e8b9e4e6ce68c
pavlo <medynskyypavlo@gmail.com>
2015-03-15 13:36:39 +0200

    Prototype v.0.1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BLogus::Application.config.secret_key_base = '42c871a6ac128072c010e15bea49138dbcac1790767bd6fd16e1d89faa51b8ffd912d54ebfba1d2905bb45ffe58c83a1ae8be76f72a279d3285230d008633a20'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BLogus::Application.config.session_store :cookie_store, key: '_bLogus_session'
